57d015b5dc23d0ea9edeefdd3cde81a2dbb70b7a
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-06-10 20:01:16 +0200

    feat(app): lint files with prettier

--- disney-characters-app/.stylelintrc.js
+++ disney-characters-app/.stylelintrc.js
@@ -3,24 +3,24 @@ module.exports = {
     'stylelint-config-standard',
     'stylelint-config-standard-scss',
     'stylelint-config-prettier-scss',
-    'stylelint-config-prettier'
+    'stylelint-config-prettier',
   ],
   plugins: ['stylelint-prettier', 'stylelint-order'],
   rules: {
-    'prettier/prettier': [true, { 'singleQuote': true, 'tabWidth': 2, 'endOfLine': 'auto' }],
+    'prettier/prettier': [true, { singleQuote: true, tabWidth: 2, endOfLine: 'auto' }],
     'order/order': ['custom-properties', 'declarations'],
     'order/properties-alphabetical-order': true,
     'selector-class-pattern': [
       '^[a-z][-a-zA-Z0-9]+$',
       {
-        'message': 'Expected custom property name to be lowerCamelCase or kebab-case'
-      }
+        message: 'Expected custom property name to be lowerCamelCase or kebab-case',
+      },
     ],
-    'at-rule-empty-line-before':[
+    'at-rule-empty-line-before': [
       'always',
       {
-        'except': ['inside-block', 'after-same-name']
+        except: ['inside-block', 'after-same-name'],
       },
-    ]
-  }
-}
+    ],
+  },
+};
--- disney-characters-app/src/App.tsx
+++ disney-characters-app/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className='App'>Disney characters app</div>;
+  return <div className="App">Disney characters app</div>;
 }
 
 export default App;
