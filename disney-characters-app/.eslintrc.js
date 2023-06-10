0a6325de109e8118f65f194f43232d5f27004329
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-06-10 16:08:25 +0200

    unextended config files for eslint, prettier due to conflicting dependencies issues

--- disney-characters-app/.eslintrc.js
+++ disney-characters-app/.eslintrc.js
@@ -1,6 +1,78 @@
 module.exports = {
-  extends: ['utility', 'utility/import'],
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json'
+    ecmaVersion: '2020',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react', '@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      2,
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/jsx-uses-react': 0,
+    'import/prefer-default-export': 0,
+    'import/extensions': 0,
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/no-unsafe-assignment': 0,
+    'no-param-reassign': 0,
+    '@typescript-eslint/no-shadow': 0,
+    '@typescript-eslint/no-unused-expressions': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-misused-promises': 1,
+    '@typescript-eslint/no-floating-promises': 1,
+    'import/order': [
+      2,
+      {
+        'newlines-between': 'never',
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      1,
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
--- disney-characters-app/.prettierrc.js
+++ disney-characters-app/.prettierrc.js
@@ -1,1 +1,10 @@
-module.exports = require('eslint-config-utility/prettier.config');
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
