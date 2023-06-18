8ffd1f8a2d096cacaa84119530a2041b4b5bc258
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-06-18 09:54:26 +0200

    feat(app): updated jest config

--- disney-characters-app/jest.config.js
+++ disney-characters-app/jest.config.js
@@ -1,10 +1,37 @@
 module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  preset: 'ts-jest',
+  bail: 1,
   verbose: true,
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
   transform: {},
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleDirectories: [
     'node_modules',
     'test',
     'src',
   ],
+  "collectCoverage": true,
+  "coverageReporters": [
+    "text"
+  ],
+  "collectCoverageFrom": [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/node_modules/",
+  ],
+  "coveragePathIgnorePatterns": [
+      "node_modules",
+      "index.tsx",
+      "<rootDir>/src/index.tsx",
+      "<rootDir>/src/reportWebVitals.ts",
+      "<rootDir>/src/hooks/useDisneyCharactersData.ts"
+  ],
+  "coverageDirectory": "<rootDir>/coverage/",
+  "coverageThreshold": {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
+  },
 };
