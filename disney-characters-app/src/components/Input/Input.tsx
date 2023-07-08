86c264bd7a222fea6438a36b63b88c11b6fe6668
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-07-08 12:10:10 +0200

    feat(app): updated input component making it reusable

--- disney-characters-app/src/components/Input/Input.tsx
+++ disney-characters-app/src/components/Input/Input.tsx
@@ -1,9 +1,9 @@
-import { ChangeEvent, FunctionComponent } from 'react';
+import { ChangeEvent, InputHTMLAttributes, forwardRef } from 'react';
 import classNames from 'classnames';
 
 import './Input.scss';
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   name?: string;
   error?: string;
@@ -13,34 +13,34 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: FunctionComponent<InputProps> = ({
-  label,
-  name,
-  error,
-  success,
-  warning,
-  disabled,
-  onChange,
-}) => (
-  <div className={classNames('inputWrapper', { disabled })}>
-    {label && (
-      <label className="inputLabel" htmlFor={name}>
-        {label}
-      </label>
-    )}
-    <input
-      className={classNames('input', {
-        'input-error': error,
-        'input-success': success,
-        'input-warning': warning,
-      })}
-      aria-label={name}
-      onChange={onChange}
-    />
-    {error && <div className="inputTextError">{error}</div>}
-    {success && <div className="inputTextSuccess">{success}</div>}
-    {warning && <div className="inputTextWarning">{warning}</div>}
-  </div>
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, name, error, success, warning, disabled, onChange, ...props }, ref) => {
+    return (
+      <div className={classNames('inputWrapper', { disabled })}>
+        {label && (
+          <label className="inputLabel" htmlFor={name}>
+            {label}
+          </label>
+        )}
+        <input
+          ref={ref}
+          className={classNames('input', {
+            'input-error': error,
+            'input-success': success,
+            'input-warning': warning,
+          })}
+          aria-label={name}
+          onChange={onChange}
+          {...props}
+        />
+        {error && <div className="inputTextError">{error}</div>}
+        {success && <div className="inputTextSuccess">{success}</div>}
+        {warning && <div className="inputTextWarning">{warning}</div>}
+      </div>
+    );
+  }
 );
+
+Input.displayName = 'Input';
 
 export default Input;
