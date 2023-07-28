a2f313d4e9625d43e0e4fa01cb3374f403b88f8f
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-07-28 21:17:12 +0200

    feat(app): fixed error in ts-fetch functions

--- disney-characters-app/src/common/api/services/fetchDisneyCharacters.ts
+++ disney-characters-app/src/common/api/services/fetchDisneyCharacters.ts
@@ -1,6 +1,6 @@
 export const getAllDisneyCharacters = async (url: string) => {
   const response = await fetch(url);
-  if (response) {
+  if (response.ok) {
     return response.json();
   }
   throw new Error('An error occurred while fetching disney characters');
